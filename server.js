95eb6295a5b5ee0f594171fc98a5096677d8bf04
Omar <omar.h@eseed.net>
2018-06-09 20:22:01 +0200

    ver 3

--- server.js
+++ server.js
@@ -1,45 +1,29 @@
-var express = require('express');
-//var app = express();
 
+var http = require('http');
 
-const app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
 
-
+const app = express()
 
 const PORT = process.env.PORT || 8080;
 
-
-
 const server = http.createServer(app);
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-//var port = process.env.PORT || 8080;
-
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
-
-// set the home page route
-app.get('/', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('index');
-});
-
-/*app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
-});*/
-
-
-
-
 
 // Start the server
-
 server.listen(PORT, () => {
-
-console.log('Server listening on: http://localhost:%s', PORT);
-
+  console.log('Server listening on: http://localhost:%s', PORT);
 });
+
+
+const apiKey = '*****************';
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  res.render('index', {out: null, error: null});
+})
+
